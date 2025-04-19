b811f90c0ae73c66e7a0de2701382ee5f68d747e
Mohd Hamza <mh230943@gmail.com>
2025-04-19 21:35:20 +0530

    Updated fetch URL to connect frontend with backend

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,8 @@ document.getElementById("orderForm").addEventListener("submit", async (e) => {
     if (num > 0) order[key] = num;
   }
 
-  const res = await fetch("http://localhost:5000/calculate-delivery-cost", {
+  // Change the URL to your Render backend
+  const res = await fetch("https://railway-cost-backend.onrender.com/calculate-delivery-cost", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(order)
